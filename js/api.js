74591b999122c361fd2866168893f820b415bef3
Andre Moreira <andreluiz.moreira@hotmail.com>
2024-06-05 21:00:15 -0300

    implementacao envio msg

--- js/api.js
+++ js/api.js
@@ -1,77 +1,70 @@
 function obterMensagens() {
-
     var retorno = [];
-
-    var consulta = $.ajax({
-        url: 'https://app-uniesp-p2-b8d2992ac568.herokuapp.com/mensagens',
-        method: 'GET',
-        dataType: 'json',
-        async: false
-    }).fail(function(){
-        return retorno;
+  
+    $.ajax({
+      url: 'https://app-uniesp-p2-b8d2992ac568.herokuapp.com/mensagens',
+      method: 'GET',
+      dataType: 'json',
+      async: true
+    }).done(function(data) {
+      retorno = data;
+    }).fail(function(erro) {
+      console.error("Erro ao obter mensagens:", erro);
     });
-
-    consulta.done(function(data) {
-        retorno = data;
-    });
-
+  
     return retorno;
-}
-
-function inserirMensagem(mensagem) {
-
-    
-
-    var mensagem = {
-            nome: "nome da pessoa", 
-            email: "email informado", 
-            mensagem: "a mensagem informada"} 
-
-    
-
-    var inserir = $.ajax({
-
-        url: 'https://app-uniesp-p2-b8d2992ac568.herokuapp.com/mensagens',
-        method: 'POST',
-        data: JSON.stringify(mensagem),
-        dataType: 'json',
-        async: false,
-        contentType: 'application/json',
+  }
+  
+  function inserirMensagem(mensagem) {
+    console.log("Enviando mensagem:", mensagem); // Adicionado para depuração
+    $.ajax({
+      url: 'https://app-uniesp-p2-b8d2992ac568.herokuapp.com/mensagens',
+      method: 'POST',
+      data: JSON.stringify(mensagem),
+      async: true,
+      contentType: 'application/json',
+      dataType: 'text', // Alterado para 'text' para evitar parsererror
+    }).done(function(data) {
+      console.log("Mensagem inserida com sucesso!", data);
+    }).fail(function(erro) {
+      console.error("Erro ao inserir mensagem:", erro);
+      console.log("Erro detalhes:", erro.responseText); // Adicionado para depuração
     });
-}
-
-function validarUsuario(objLoginSenha) {
-
-    email: admin@admin.com
-    senha: '1234'
-
-    
-
-    var objLoginSenha = {
-            email: "email informado", 
-            senha: "senha informada"} 
-
-    
-
-    var retorno = false;
-
-    var validacao = $.ajax({
-        url: 'https://app-uniesp-p2-b8d2992ac568.herokuapp.com/usuarios/validar',
-        method: 'POST',
-        dataType: 'json',
-        async: false,
-        headers: {
-            'Access-Control-Allow-Origin': '*'
-                },
-        contentType: 'application/json',
-        data: JSON.stringify(objLoginSenha)
-    }).fail(function(){
-        return retorno;
+  }
+  
+  function validarUsuario(objLoginSenha) {
+    $.ajax({
+      url: 'https://app-uniesp-p2-b8d2992ac568.herokuapp.com/usuarios/validar',
+      method: 'POST',
+      dataType: 'json',
+      async: true,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      contentType: 'application/json',
+      data: JSON.stringify(objLoginSenha)
+    }).done(function(data) {
+      retorno = data;
+    }).fail(function(erro) {
+      console.error("Erro ao validar usuário:", erro);
     });
-
-    validacao.done(function(data) {
-        retorno = data;
-    });
-
+  
     return retorno;
-}
+  }
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('send-button').addEventListener('click', function(event) {
+      event.preventDefault();
+  
+      const nome = document.getElementById('nome').value;
+      const email = document.getElementById('email').value;
+      const mensagem = document.getElementById('msg').value;
+  
+      if (nome && email && mensagem) {
+        inserirMensagem({ nome, email, mensagem });
+      } else {
+        alert('Por favor, preencha todos os campos.');
+      }
+    });
+  });
+  
